ef6fe439e0c370e84279b918569754eb103ec19c
Mujeeb Ahmed <82079639+mujeebahmeddev@users.noreply.github.com>
2023-12-16 12:14:02 +0000

    updated Arithmatic operators content in Day-3

--- Week-1/Day-3.js
+++ Week-1/Day-3.js
@@ -99,5 +99,119 @@ Objects, Arrays etc
 // NOTE: We will learn about the Non-Primitive Datatype in the further Topics
 
 
+// Operators in JS
+
+// JavaScript **operators** operate the operands, these are symbols that are used to manipulate a certain value or operand. 
+
+// Operators are used to performing specific mathematical and logical computations on operands.
+
+// There are different types of JavaScript operators:
+
+// - Arithmetic Operators
+// - Assignment Operators
+// - Comparison Operators
+// - String Operators
+// - Logical Operators
+// - Bitwise Operators
+// - Ternary Operators
+
+// - Arithmetic Operators : JavaScript arithmetic operators perform mathematical calculations on numbers. 
+
+/*
+1. Basic Operators:
+    - `+`: Addition
+    - `-`: Subtraction
+    - `*`: Multiplication
+    - `/`: Division
+    - `%`: Modulus (remainder)
+2. Unary Operators:
+    - `++`: Increment (increases by 1)
+    - `-`: Decrement (decreases by 1)
+3. Exponentiation Operator:
+    - `*`: Exponentiation (introduced in ES2016)
+*/
+
+// Examples
+
+
+// // 1. +: Addition : The addition operator (+) adds numbers.
+
+// let num1 = 10;
+// let num2 = 5;
+
+// // Addition
+// let sum = num1 + num2; 
+// console.log(sum); // Output will be 15
+
+
+
+// 2. - : Subtraction : The subtraction operator (-) subtracts numbers.
+// let num1 = 10;
+// let num2 = 5;
+
+
+// // Subtraction
+// let difference = num1 - num2;
+// console.log(difference); // 5
+
+
+// 3. *: Multiplication : The multiplication operator (*) multiplies numbers.
+
+// let num1 = 10;
+// let num2 = 5;
+
+// // // Multiplication
+// let product = num1 * num2; 
+// console.log(product);// 50
+
+// 4. /: Division : The division operator (/) divides numbers.
+// let num1 = 10;
+// let num2 = 5;
+
+// // Division
+// let quotient = num1/ num2; 
+// console.log(quotient);// 2
+
+
+// 5. %: Modulus (remainder) : The modulus operator (%) returns the division remainder.
+
+// let num1 = 10;
+// let num2 = 5;
+
+// // Modulus
+// let remainder = num1 % num2;
+// console.log(remainder); // 0
+
+// 6. ++: Increment (increases by 1)  : The increment operator (++) increments numbers.
+
+// let num1 = 1;
+
+// // Increment
+// num1++
+// console.log(num1);// x becomes 2
+
+// ++num1
+// console.log(num1);// x becomes 3
+
+// let num1 = 10;
+
+
+// // Decrement
+// num1--; // y becomes 9
+// console.log(num1);
+// --num1; // y becomes 8
+// console.log(num1);
+
+
+
+// **: Exponentiation (introduced in ES2016) : The exponentiation operator (**) raises the first operand to the power of the second operand.
+
+
+// let num1 = 5;
+// let num2 = 2;
+
+// // Exponentiation
+// let power = num1 ** num2; 
+// console.log(power);// 5*5 = 25
 
 
